e4a57ef7f8793e741c9e5c6e3bcc1f57de4b9e32
RedAlexDad <boss6852@gmail.com>
2023-09-29 16:31:19 +0300

    Добавлен config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+# Для облачного сервиса
+# import os
+
+# Токен для API телеграм бот
+TG_BOT_TOKEN = ''
+# Для облачного сервиса
+# TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+
+# Для доступа к облачному сервису - Yandex Could
+IAM_TOKEN = ''
+# Для облачного сервиса
+# IAM_TOKEN = os.environ.get('IAM_TOKEN')
+
+# Идентификатор каталога (тоже предоставляет доступ к каталогу сервера)
+FOLDER_ID = ''
+# Для облачного сервиса
+# FOLDER_ID = os.environ.get('FOLDER_ID')
+
+# ChatGPT
+CHAT_GPT_API_KEY = ''
+# Для облачного сервиса
+# CHAT_GPT_API_KEY = os.environ.get('CHAT_GPT_API_KEY')
+
+# Конфигурация с БД Yandex PostgreSQL
+database = ''
+host = ''
+port = 5432
+sslmode = ''
+user = ''
+password = ''
+dbname = ''
+target_session_attrs = ''
+
+# Yandex Object Storage
+bucket_name = ''
+aws_access_key_id = ''
+aws_secret_access_key = ''
+region_name = ''
+
+# Эмоциональные окраски и метрика
+API_TOKEN_EMOTION = ''
+API_URL_EMOTION = ''
+
